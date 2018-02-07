ddba6e7d97fee8778575b322bb6fd39cee05e32d
Per-Axel Felth <per-axel.felth@teliacompany.com>
2018-02-07 07:41:18 +0100

    clean up

--- src/main/java/com/teliacarrier/payarapathbug/ApplicationConfig.java
+++ src/main/java/com/teliacarrier/payarapathbug/ApplicationConfig.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.teliacarrier.payarapathbug;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-/**
- *
- * @author jnj112
- */
 @ApplicationPath("/")
 public class ApplicationConfig extends Application {
     
--- src/main/java/com/teliacarrier/payarapathbug/Test2Resource.java
+++ src/main/java/com/teliacarrier/payarapathbug/Test2Resource.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.teliacarrier.payarapathbug;
 
 import javax.ws.rs.GET;
@@ -10,10 +5,6 @@ import javax.ws.rs.OPTIONS;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 
-/**
- *
- * @author jnj112
- */
 @Path("test2")
 public class Test2Resource {
     @GET
